42b62e0ff62ffa349608e651d3328263a43fbed0
Nicolas <nicolas.anascimento@gmail.com>
2018-09-17 11:59:23 -0300

    aula 1 de TS

--- TYPESCRIPT/01/app.ts
+++ TYPESCRIPT/01/app.ts
@@ -2,6 +2,12 @@ let message: string = "Help me, Obi-Wan Kenobi. You're my only Hope!";
 console.log(message);
 
 let episode: number = 4;
+// episode = '4';
 console.log("This is episode " + 4);
 episode = episode + 1;
 console.log("Next episode is " + episode);
+
+let favoriteDroid: string;
+favoriteDroid = 'BB-8';
+// favoriteDroid = 10;
+console.log(favoriteDroid);
