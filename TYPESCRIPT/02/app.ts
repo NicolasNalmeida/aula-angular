21c97a644f04daf4fa23e2c67f2742509bf9313e
Nicolas <nicolas.anascimento@gmail.com>
2018-09-17 13:09:38 -0300

    funções

--- TYPESCRIPT/02/app.ts
+++ TYPESCRIPT/02/app.ts
@@ -1,1 +1,8 @@
 //FUNÇÕES
+let isEnoughToBeatMF = function(parsecs: number): boolean{
+    return parsecs < 12;
+}
+
+let distance: number = 14;
+
+console.log(`Is ${distance} parsecs enough to beat MF? ${isEnoughToBeatMF(distance) ? 'Yes': 'No'}`);
--- TYPESCRIPT/02/dist/app.js
+++ TYPESCRIPT/02/dist/app.js
@@ -1,1 +1,6 @@
 //FUNÇÕES
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 14;
+console.log("Is " + distance + " parsecs enough to beat MF? " + (isEnoughToBeatMF(distance) ? 'Yes' : 'No'));
