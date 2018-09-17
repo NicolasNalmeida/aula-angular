13c9e9aca2f7502c19208627d67af6670be08e00
Nicolas <nicolas.anascimento@gmail.com>
2018-09-17 13:18:04 -0300

    arrow function e parâmetros

--- TYPESCRIPT/02/app.ts
+++ TYPESCRIPT/02/app.ts
@@ -6,3 +6,16 @@ let isEnoughToBeatMF = function(parsecs: number): boolean{
 let distance: number = 14;
 
 console.log(`Is ${distance} parsecs enough to beat MF? ${isEnoughToBeatMF(distance) ? 'Yes': 'No'}`);
+
+//ARROW FUNCTION
+let call = (name: string) => console.log(`Do you copy, ${name}`);
+call('R2');
+
+//PARÂMETROS PADRÕES EM TS
+
+function inc(speed:number, inc: number = 1): number{
+    return speed + inc;
+}
+
+console.log(`inc(5, 1) = ${inc(5, 1)}`); //passando valores em todos os parâmetros da função
+console.log(`inc(5) = ${inc(5)}`); //passando valores com apenas um parâmetro
--- TYPESCRIPT/02/dist/app.js
+++ TYPESCRIPT/02/dist/app.js
@@ -4,3 +4,13 @@ var isEnoughToBeatMF = function (parsecs) {
 };
 var distance = 14;
 console.log("Is " + distance + " parsecs enough to beat MF? " + (isEnoughToBeatMF(distance) ? 'Yes' : 'No'));
+//ARROW FUNCTION
+var call = function (name) { return console.log("Do you copy, " + name); };
+call('R2');
+//PARÂMETROS PADRÕES EM TS
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc(5, 1) = " + inc(5, 1)); //passando valores em todos os parâmetros da função
+console.log("inc(5) = " + inc(5)); //passando valores com apenas um parâmetro
