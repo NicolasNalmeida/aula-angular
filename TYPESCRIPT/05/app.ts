df288e42a7aa33fa3ba25d0b505daf472ac8ddd7
Nicolas <nicolas.anascimento@gmail.com>
2018-09-17 15:51:05 -0300

    import biblioteca

--- TYPESCRIPT/05/app.ts
+++ TYPESCRIPT/05/app.ts
@@ -1,6 +1,11 @@
 import {Spacecraft, Containership} from './base-ships';
 import { MilleniumFalcon } from './starfighters';
 
+import * as _ from 'lodash'
+
+console.log(_.pad("Typescript Examples", 40, "="));
+
+
 let ship = new Spacecraft('hyperdrive'); // instanciando a classe Spacecraft passando um valor por parâmetro atribundo esse valor ao construtor
 ship.jumpInToHyperspace(); //executando o método
 
